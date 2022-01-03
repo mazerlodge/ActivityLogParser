4b0f5d51966f3c427a4be62685174f6045da6770
Paul Soren <mazerlodge@hotmail.com>
2022-01-03 12:23:42 -0600

    Update to current version 20220103

--- ActivityLogParser.py
+++ ActivityLogParser.py
@@ -1,37 +1,13 @@
 #!/usr/local/bin/python3
 
-import sys 
-from ArgTools import ArgParser
-
-def debugMsg(msg): 
-	global bInDebug
-
-	if (bInDebug):
-		print(msg) 
-
-
 # vars
-inFilename = "./data/in.txt"
-outFilename = "NOT_SET"
-bInDebug = False
+fn = "./data/in.txt"
 tagDict = {}
-ap = ArgParser(sys.argv)
-
-# check command line for input and/or output filename(s) 
-if (ap.isInArgs("-in", True)):
-	inFilename = ap.getArgValue("-in")
-
-if (ap.isInArgs("-out", True)):
-	outFilename = ap.getArgValue("-out")
-
-# check command line for debug mode
-if (ap.isInArgs("-debug", False)): 
-	bInDebug = True
 
 # Read the activity log
-inFile = open(inFilename, "r")
-lines = inFile.readlines()
-inFile.close()
+file = open(fn, "r")
+lines = file.readlines()
+file.close()
 
 # group lines from the log according to the line tag. 
 # Note: line tag = start of line text between dash and comma.
@@ -46,13 +22,13 @@ for aLine in lines:
 			# is current categoryTag in the dictionary already
 			categoryList  = tagDict.get(categoryTag)
 			if categoryList == None: 
-				debugMsg("New category")
+				print("New category")
 				categoryList = []
 				categoryList.append(outLine)  
 				tagDict[categoryTag] = categoryList 
 			else: 
 				categoryList.append(outLine)
-				debugMsg("Added, list now %d to category [%s]" % (len(categoryList), outLine))
+				print("Added, list now %d to category [%s]" % (len(categoryList), outLine))
 
 # Output lines grouped by tags
 for aKey in tagDict: 
@@ -60,20 +36,3 @@ for aKey in tagDict:
 	aList = tagDict[aKey]
 	for aLine in aList:
 		print("\t%s" % aLine) 
-	print("")
-
-# Prepare the output file if one was specified 
-if (outFilename != "NOT_SET"): 
-	outFile = open(outFilename, "w")
-	for aKey in tagDict: 
-		outLine = "%s\n" % aKey
-		outFile.write(outLine)
-		aList = tagDict[aKey]
-		for aLine in aList:
-			outLine = "\t%s\n" % aLine
-			outFile.write(outLine)
-		outFile.write("\n")
-
-	outFile.flush() 
-	outFile.close()
-	
